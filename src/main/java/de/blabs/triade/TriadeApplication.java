ae5e438de971f9ea531793c94c0bacabe0793999
Bastian <bastib83@gmail.com>
2021-03-03 12:03:18 +0100

    initialize new springboot app

---
+++ src/main/java/de/blabs/triade/TriadeApplication.java
@@ -0,0 +1,13 @@
+package de.blabs.triade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/blabs/triade/TriadeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.blabs.triade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
